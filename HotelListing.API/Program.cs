b7af45dd281fa7572c42b9480bf3077d00fbf8d5
MiGo75Git <54631111+MiGo75Git@users.noreply.github.com>
2022-07-12 23:49:33 +0200

    Data Classes and EF Perform Migrations

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using HotelListing.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connString);
+});
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
