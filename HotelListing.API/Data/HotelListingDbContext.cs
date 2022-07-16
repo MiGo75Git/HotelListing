549a18ed9cf4f0b6e654ae42098b9df04643634f
MiGo75Git <54631111+MiGo75Git@users.noreply.github.com>
2022-07-16 23:00:10 +0200

    default User Roles, arranged Data seeding

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelListing.API.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Data
@@ -10,95 +11,15 @@ namespace HotelListing.API.Data
 
         }
         public DbSet<Hotel> Hotels { get; set; }
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<ApiUser> ApiUsers { get; set; }
+        public DbSet<Country> Countries { get; set; }      
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>().HasData(
-                new Country
-                {
-                    Id = 1,
-                    Name = "Jamaica",
-                    ShortName = "JM"
-                },
-                new Country
-                {
-                    Id = 2,
-                    Name = "Bahamas",
-                    ShortName = "BS"
-                },
-                new Country
-                {
-                    Id = 3,
-                    Name = "Cayman Island",
-                    ShortName = "CI"
-                },
-                new Country
-                {
-                    Id = 4,
-                    Name = "Slovenia",
-                    ShortName = "SI"
-                },
-                new Country
-                {
-                    Id = 5,
-                    Name = "Österrich",
-                    ShortName = "AT"
-                }
-            );
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
 
-            modelBuilder.Entity<Hotel>().HasData(
-                new Hotel
-                {
-                    Id = 1,
-                    Name = "Sandals Resort and Spa",
-                    Address = "Negril",
-                    CountryId = 1,
-                    Rating = 4.5
-                },
-                new Hotel
-                {
-                    Id = 2,
-                    Name = "Comfort Suites",
-                    Address = "George Town",
-                    CountryId = 3,
-                    Rating = 4.3
-                },
-                new Hotel
-                {
-                    Id = 3,
-                    Name = "Grand Palldium",
-                    Address = "Nassua",
-                    CountryId = 2,
-                    Rating = 4
-                },
-                new Hotel
-                {
-                    Id = 4,
-                    Name = "Zlatorog ",
-                    Address = "Bohinj",
-                    CountryId = 4,
-                    Rating = 5
-                },
-                new Hotel
-                {
-                    Id = 5,
-                    Name = "Slavija",
-                    Address = "Maribor",
-                    CountryId = 4,
-                    Rating = 3
-                },
-                new Hotel
-                {
-                    Id = 6,
-                    Name = "Maribor",
-                    Address = "Maribor",
-                    CountryId = 4,
-                    Rating = 4.1
-                }
-            );
         }
 
 
